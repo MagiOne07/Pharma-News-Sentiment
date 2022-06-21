5f4d714add75adb06fd9c3e283893f24bed9f827
MagiOne07 <74280223+MagiOne07@users.noreply.github.com>
2022-06-21 21:18:14 +0100

    Add files via upload

---
+++ phrama_sentiment.py
@@ -0,0 +1,50 @@
+import streamlit as st
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+vader = SentimentIntensityAnalyzer()
+import numpy as np
+key_words = np.array(['Anticoagulants', 'Heparin', 'Enoxaparin','Fragmin','Lovenox','Vaccine','Flu','Pneumo','Travel vaccine','Covid-19 vaccine','Pediatric vaccine','Hyaluronic acid','small molecules','Vials to Prefillable' ,'Trulicity','Humira','Eylea','Copaxone','ozempic','Enbrel','Prevnar','Gardasil','Zarxio','Pegasys'])
+import pandas as pd
+df = pd.DataFrame(index=key_words, columns=['headline'])
+import pandas as pd
+df = pd.DataFrame(index=key_words, columns=['headline'])
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://news.google.com/search?q='
+for word in key_words:
+    headlines = []
+    response = requests.get(url+word+' when:7d', headers={
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"})
+    soup = BeautifulSoup(response.text, 'lxml')
+    data = soup.find_all('a', {'class':'DY5T1d RZIKme'})
+    for i in range(len(data)):
+        headlines.append(data[i].text)
+    df.loc[word, 'headline'] = headlines
+scores=[]
+for c in range(len(df['headline'])):
+    score=[]
+    for k in df['headline'][c]:
+        score.append(vader.polarity_scores(k))
+    score_mean = np.array([score[com]['compound'] for com in range(len(score))]).mean()
+    scores.append(score_mean)
+df['score']=scores
+df['positive'] = df['score']>0
+#print(df)
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+#plt.style.use("classic")
+#plt.xticks(fontsize=25)
+#plt.yticks(fontsize=15)
+#plt.title('Key Word Sentiment Analysis')
+#df['score'].plot(kind='bar',figsize = (20, 10),color=df.positive.map({True: 'g', False: 'r'}))
+import altair as alt
+new=df[['score']]
+new['y']=new.index
+new['pos']=df['positive']
+chart=alt.Chart(new).mark_bar().encode(x='score',y='y',color=alt.condition(alt.datum.pos == True, alt.value('green'), alt.value('red')))
+st.header('Pharma News Sentiment')
+st.altair_chart(chart, use_container_width=True)
+key=st.selectbox('Select Option', (key_words))
+st.write(df.loc[key,'headline'])
+del(i,c,k,word,)
